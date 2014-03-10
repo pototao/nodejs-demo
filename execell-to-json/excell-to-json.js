848c4d45ca6ced245c0d61d73725725527c25cf3
koala <koalaylj@gmail.com>
2014-03-10 17:34:18 +0800

    修改excell导出json工具 导出方式 提示信息更友好。

--- execell-to-json/excell-to-json.js
+++ execell-to-json/excell-to-json.js
@@ -7,10 +7,10 @@ var dir_dst = './json/';
 var postfix = '.xlsx';
 
 function walk(dir) {
-	var dir_list = fs.readdirSync(dir);
+	var files_under_dir = fs.readdirSync(dir);
 	var files = [];
 
-	dir_list.forEach(function(item) {
+	files_under_dir.forEach(function(item) {
 		if (fs.statSync(dir + item).isFile()) {
 			if (path.extname(item) == postfix) {
 				files.push(item);
@@ -23,14 +23,30 @@ function walk(dir) {
 
 var files_under_dir = walk(dir_src);
 
-for (var i = 0; i < files_under_dir.length; i++) {
-	console.log('exporting...', files_under_dir[i]);
-	xlsx_json({
-		input: dir_src + files_under_dir[i],
-		output: dir_dst + path.basename(files_under_dir[i], postfix) + '.json'
-	}, function(err, result) {
-		if (err) {
-			console.error(err);
-		} else {}
-	});
-};
+files_under_dir.forEach(
+	function(filename) {
+		var output_file = path.basename(filename, postfix) + '.json';
+		xlsx_json({
+			input: dir_src + filename,
+			output: dir_dst + output_file
+		}, function(err, result) {
+			if (err) {
+				console.error(err);
+			} else {
+				console.log("Exported:", filename + " --> " + output_file);
+			}
+		});
+	}
+);
+
+// for (var i = 0; i < files_under_dir.length; i++) {
+// 	console.log('exporting...', files_under_dir[i]);
+// 	xlsx_json({
+// 		input: dir_src + files_under_dir[i],
+// 		output: dir_dst + path.basename(files_under_dir[i], postfix) + '.json'
+// 	}, function(err, result) {
+// 		if (err) {
+// 			console.error(err);
+// 		} else {}
+// 	});
+// };
